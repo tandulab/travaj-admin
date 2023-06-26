abca53d1ecbb9cf3e64b2e2f8fa38928b5f38380
Sara <sarabertaina@hotmail.it>
2023-06-26 11:48:13 +0200

    build 0.0.58

---
+++ assets/Logo54837.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./vee-validate.esm54837.js";import{_ as e,b as r,e as t}from"../index54837.js";const c={},s={src:o,alt:"Logo"};function _(n,a){return r(),t("img",s)}const f=e(c,[["render",_]]);export{f as L};
